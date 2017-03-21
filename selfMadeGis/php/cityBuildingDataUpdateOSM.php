1bd02cbe823e5a25de5ca090e6448ecafe041551
Yurii <u.shpilevoy@gmail.com>
2017-03-21 16:13:26 +0200

    update 21.03.2017
    - third part of the scripts transfer to class type connection to DB

--- selfMadeGis/php/cityBuildingDataUpdateOSM.php
+++ selfMadeGis/php/cityBuildingDataUpdateOSM.php
@@ -1,55 +1,35 @@
 <?php
 //ini_set('display_errors', 1);
-//ini_set('max_execution_time', 0);
-      $selectedCity= $_POST['city_building_OSM_data_eng'];    
-      if (file_exists("/tmp/".$selectedCity."_buildings_osm.csv")) {
-        $linkStorage = "'/tmp/".$selectedCity."_buildings_osm.csv'";
-        $dir = sys_get_temp_dir();
-        $files = scandir($dir);  
-      } else {
-        $linkStorage = "'/var/www/QGIS-Web-Client-master/site/csv/archive/".$selectedCity."/".$selectedCity."_buildings_osm.csv'" ;
-        $dir = "/var/www/QGIS-Web-Client-master/site/csv/archive/".$selectedCity."/";
-        $files = scandir($dir);
-      }
-      //echo $linkStorage;
-      //echo '<hr>'. file_exists($linkStorage);
-           $host        = "host=127.0.0.1";
-           $port        = "port=5432";
-           $dbname      = "dbname=postgres";
-           $credentials = "user=simpleuser password=simplepassword";
-
-           $db = pg_connect( "$host $port $dbname $credentials"  );
-             if(!$db){
-                echo "Error : Unable to open database\n";
-             } else {
-                if ($files) {
-                  foreach($files as $file) {
-                    $str_file = (string)$file;
-                    if ($str_file !== '.' && $str_file !== '..') {
-                          print_r($str_file);
-                          if ($str_file == $selectedCity."_buildings_osm.csv") {
-                            
-                         $insert = "CREATE temp TABLE tmp (id serial, temp text,openstreet_id_rel varchar(100),openstreet_doggy_id_rel  varchar(100),openstreet_addr_housenumber varchar(100),openstreet_addr_street varchar(100), openstreet_amenity varchar(100),openstreet_building_type varchar(100), openstreet_building_levels varchar(100)); select copy_for_testuser('tmp (temp, openstreet_id_rel, openstreet_doggy_id_rel,openstreet_addr_housenumber, openstreet_addr_street, openstreet_amenity, openstreet_building_type, openstreet_building_levels)', ".$linkStorage.", ';', 'windows-1251') ;  INSERT INTO ".$selectedCity.".".$selectedCity."_buildings(temp, openstreet_id_rel, openstreet_doggy_id_rel,openstreet_addr_housenumber, openstreet_addr_street, openstreet_amenity, openstreet_building_type, openstreet_building_levels) SELECT  temp, openstreet_id_rel, openstreet_doggy_id_rel,openstreet_addr_housenumber, openstreet_addr_street, openstreet_amenity, openstreet_building_type, openstreet_building_levels FROM tmp WHERE openstreet_id_rel NOT IN(SELECT openstreet_id_rel FROM ".$selectedCity.".".$selectedCity."_buildings WHERE openstreet_id_rel IS NOT NULL);DROP TABLE tmp;";
-                          $ret = pg_query($db, $insert);
-                          $update_geom = "UPDATE ".$selectedCity.".".$selectedCity."_buildings SET building_geom = ST_GeomFromText(temp, 32636) WHERE building_geom IS NULL;";
-                          $geo_ret = pg_query($db, $update_geom);
-                         
-
-                         } else {
-                            echo ' error cannot update data ';
-                          }
-            }
-          } 
-
-        }
-
-	}
-  //-----------------------------------------------------------------
+include('classFunctionStorage.php');
   
-
-
-
-	pg_close($db); // Closing Connection
-	print( $insert.$update_geom);
+if ($_POST['city_building_OSM_data_eng']) {
+  $selectedCity= $_POST['city_building_OSM_data_eng'];
+} else {
+  $selectedCity = $_REQUEST['city_building_OSM_data_eng'];
+}  
+  $promeLink = "/tmp/";
+  $secondaryLink = "/var/www/QGIS-Web-Client-master/site/csv/archive/";
+  $fileExtention ="_buildings_osm.csv";
+  $files = fileExistenceCheck($promeLink, $secondaryLink, $selectedCity, $fileExtention)['files'];
+  $linkStorage = fileExistenceCheck($promeLink, $secondaryLink, $selectedCity, $fileExtention)['linkStorage'];
+if ($files) {
+  foreach($files as $file) {
+    $str_file = (string)$file;
+    if ($str_file !== '.' && $str_file !== '..') {
+      //print_r($str_file);
+      if ($str_file == $selectedCity.$fileExtention) {
+        $newDBrequest = new dbConnSetClass;
+        $query = "CREATE temp TABLE tmp (id serial, temp text,openstreet_id_rel varchar(100),openstreet_doggy_id_rel  varchar(100),openstreet_addr_housenumber varchar(100),openstreet_addr_street varchar(100), openstreet_amenity varchar(100),openstreet_building_type varchar(100), openstreet_building_levels varchar(100)); select copy_for_testuser('tmp (temp, openstreet_id_rel, openstreet_doggy_id_rel,openstreet_addr_housenumber, openstreet_addr_street, openstreet_amenity, openstreet_building_type, openstreet_building_levels)', ".$linkStorage.", ';', 'windows-1251') ;  INSERT INTO ".$selectedCity.".".$selectedCity."_buildings(temp, openstreet_id_rel, openstreet_doggy_id_rel,openstreet_addr_housenumber, openstreet_addr_street, openstreet_amenity, openstreet_building_type, openstreet_building_levels) SELECT  temp, openstreet_id_rel, openstreet_doggy_id_rel,openstreet_addr_housenumber, openstreet_addr_street, openstreet_amenity, openstreet_building_type, openstreet_building_levels FROM tmp WHERE openstreet_id_rel NOT IN(SELECT openstreet_id_rel FROM ".$selectedCity.".".$selectedCity."_buildings WHERE openstreet_id_rel IS NOT NULL);DROP TABLE tmp;";
+        $queryArrayKeys = false;
+        //echo $query;
+        $retuenedArray = $newDBrequest -> dbConnect($query, $queryArrayKeys, true);
+        $query = "UPDATE ".$selectedCity.".".$selectedCity."_buildings SET building_geom = ST_GeomFromText(temp, 32636) WHERE building_geom IS NULL;";
+        $queryArrayKeys = false;
+        //echo $query;
+        $retuenedArray = $newDBrequest -> dbConnect($query, $queryArrayKeys, true);
+      }
+    } 
+  }
+}
 ?>
 
--- selfMadeGis/php/cityBuildingDublicatesFinder.php
+++ selfMadeGis/php/cityBuildingDublicatesFinder.php
@@ -1,51 +1,32 @@
 <?php
 //ini_set('display_errors', 1);
-
- 	$selectedCity= $_POST['city_building_dublicates_finder_eng'];  
-
-           $host        = "host=127.0.0.1";
-           $port        = "port=5432";
-           $dbname      = "dbname=postgres";
-           $credentials = "user=simpleuser password=simplepassword";
-
-           $db = pg_connect( "$host $port $dbname $credentials"  );
-             if(!$db){
-                echo "Error : Unable to open database\n";
-             } else {
-                //echo "Opened database successfully\n";
-            // echo "Opened database successfully\n";
-	}
-
- 
-                 $sql = "SELECT openstreet_addr_street, openstreet_addr_housenumber, cubic_street, cubic_house, cubic_house_id, cubic_network_type, ST_AsLatLonText(ST_Transform(building_geom_firstpoint, 4326) ) FROM ".$selectedCity.".".$selectedCity."_buildings WHERE cubic_house_id IN (SELECT cubic_house_id FROM  (SELECT openstreet_addr_street, openstreet_addr_housenumber,cubic_house_id, count(*) AS count FROM ".$selectedCity.".".$selectedCity."_buildings  WHERE cubic_house_id IS NOT NULL GROUP BY 1, openstreet_addr_street, openstreet_addr_housenumber,cubic_house_id  ORDER BY count) count WHERE count >1) ORDER BY cubic_house_id, openstreet_addr_street, openstreet_addr_housenumber";
-                  $ret = pg_query($db, $sql);
-                  $arr_response = array('response' => array());
-                  if($ret) {
-
-                         while ($row = pg_fetch_row($ret))  {
-
-                          $arr = array(
-                              'openstreet_addr_street' => $row[0],
-                              'openstreet_addr_housenumber' => $row[1],
-                              'cubic_street' => $row[2],
-                              'cubic_house' => $row[3],
-                              'cubic_house_id' => $row[4],
-                              'cubic_network_type' => $row[5],
-                              'building_geom_firstpoint' => $row[6]
-                            );
-                          //print_r( $arr);
-                          array_push($arr_response['response'], $arr );
-                          //array_push($arr_response, $arr);
-                        }
-                  }
-
-     
-      
-     
-
-	
-	print json_encode($arr_response);
-	pg_close($db); // Closing Connection
-	//print( $sql);
+include('classFunctionStorage.php');
+  
+if ($_POST['city_building_dublicates_finder_eng']) {
+  $selectedCity= $_POST['city_building_dublicates_finder_eng'];
+} else {
+  $selectedCity = $_REQUEST['city_building_dublicates_finder_eng'];
+}
+$newDBrequest = new dbConnSetClass;
+$query = "SELECT openstreet_addr_street, openstreet_addr_housenumber, cubic_street, cubic_house, cubic_house_id, cubic_network_type, ST_AsLatLonText(ST_Transform(building_geom_firstpoint, 4326) ) as coords FROM ".$selectedCity.".".$selectedCity."_buildings WHERE cubic_house_id IN (SELECT cubic_house_id FROM  (SELECT openstreet_addr_street, openstreet_addr_housenumber,cubic_house_id, count(*) AS count FROM ".$selectedCity.".".$selectedCity."_buildings  WHERE cubic_house_id IS NOT NULL GROUP BY 1, openstreet_addr_street, openstreet_addr_housenumber,cubic_house_id  ORDER BY count) count WHERE count >1) ORDER BY cubic_house_id, openstreet_addr_street, openstreet_addr_housenumber;";
+//echo $query;
+$queryArrayKeys = array('openstreet_addr_street', 'openstreet_addr_housenumber', 'cubic_street', 'cubic_house', 'cubic_house_id', 'cubic_network_type', 'coords');
+$retuenedArray = $newDBrequest -> dbConnect($query, $queryArrayKeys, true);
+$sumObjectsArray = $retuenedArray;
+//print_r($sumObjectsArray);
+$arr_response = array('response' => array());
+foreach ($sumObjectsArray as $sumObjectsArrayKey => $objectArray) {
+  $arr = array(
+    'openstreet_addr_street' => $sumObjectsArray[$sumObjectsArrayKey]['openstreet_addr_street'],
+    'openstreet_addr_housenumber' => $sumObjectsArray[$sumObjectsArrayKey]['openstreet_addr_housenumber'],
+    'cubic_street' => $sumObjectsArray[$sumObjectsArrayKey]['cubic_street'],
+    'cubic_house' => $sumObjectsArray[$sumObjectsArrayKey]['cubic_house'],
+    'cubic_house_id' => $sumObjectsArray[$sumObjectsArrayKey]['cubic_house_id'],
+    'cubic_network_type' => $sumObjectsArray[$sumObjectsArrayKey]['cubic_network_type'],
+    'building_geom_firstpoint' => $sumObjectsArray[$sumObjectsArrayKey]['coords']
+  );
+  array_push($arr_response['response'], $arr ); 
+}
+print json_encode($arr_response);
 ?>
 
