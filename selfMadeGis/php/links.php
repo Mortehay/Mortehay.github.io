7d54dd142b287864ed035221e88d4e24c1148f9b
Yurii <u.shpilevoy@gmail.com>
2017-06-09 10:26:20 +0300

    update 09.06.2017
    - upgraded links script with classes;
    - added first part trigger/function =>entrance point to poligon on
    geometry change;
    - added save to csv plugin to qgis(saved csv can be opened in excel)

--- selfMadeGis/php/links.php
+++ selfMadeGis/php/links.php
@@ -1,62 +1,18 @@
 <?php
-
-function postgres_to_php_array($postgresArray) {
-
-   $postgresStr = trim($postgresArray,"{}");
-    $elmts = explode(",",$postgresStr);
-    return $elmts;
-
-  }
-
-//-------------------------------------------
-header("Access-Control-Allow-Origin: *");
-ini_set('display_errors', 1);
-   $host        = "host=127.0.0.1";
-   $port        = "port=5432";
-   $dbname      = "dbname=postgres";
-   $credentials = "user=simpleuser password=simplepassword";
-
- $db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db){
-      echo "Error : Unable to open database\n";
-   } else {
-      //echo "Opened database successfully\n";
-   }
- $sql = "SELECT id, city, links, links_description   FROM public.links WHERE links IS NOT NULL ORDER BY city";
- $ret = pg_query($db, $sql);
-   if(!$ret){
-      echo pg_last_error($db);
-   } else {
-      //echo "Records created successfully\n";
-   }
-
-   
-   //$arr = pg_fetch_all($ret);
-
-      // $geojson = array( 'type' => 'FeatureCollection', 'features' => array());
-       $json = array();
-    
-       
-         while ($row = pg_fetch_row($ret))  {
-
-          
-              $arr = array(
-                  
-                    'id' => (int)$row[0],
-                    'city'=>$row[1],
-                    'links'=> postgres_to_php_array($row[2]), //$row[2],postgres_to_php_array($row[2])
-                    'links_description'=>postgres_to_php_array($row[3]),
-                   
-              );
-              //echo $row[2] ;
-            array_push($json, $arr);
-
-          
-              
-        }
-   
-
-   print json_encode($json);
-
-   pg_close($db);
+include('classFunctionStorage.php');
+$query = "SELECT e_mail, restriction FROM public.access;";
+//echo $query;
+$queryArrayKeys = array('id', 'city', 'links', 'links_description');
+$arr_response = array('response' => array());
+$retuenedArray = $newDBrequest -> dbConnect($query, $queryArrayKeys, true);
+foreach ($sumObjectsArray as $sumObjectsArrayKey => $objectArray) {
+  $arr = array(
+    'id' => (int)$sumObjectsArray[$sumObjectsArrayKey]['id'],
+    'city' => $sumObjectsArray[$sumObjectsArrayKey]['city'],
+    'links' => postgres_to_php_array($sumObjectsArray[$sumObjectsArrayKey]['links']),
+    'links_description' => postgres_to_php_array($sumObjectsArray[$sumObjectsArrayKey]['links_description'])
+  );
+  array_push($arr_response['response'], $arr ); 
+}
+print json_encode($arr_response);
 ?>
